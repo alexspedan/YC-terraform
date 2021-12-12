4532c5da3301e693c44b13cbd93cbb1c9254d43d
Aleksey Pedan <35327043+alexspedan@users.noreply.github.com>
2021-12-12 21:07:12 +0300

    updated to 13 ver

--- db.tf
+++ db.tf
@@ -12,7 +12,7 @@ resource "yandex_mdb_postgresql_cluster" "wp_postgresql" {
   network_id  = yandex_vpc_network.wp-network.id
 
   config {
-    version = 12
+    version = 13
     resources {
       resource_preset_id = "s2.small"
       disk_type_id       = "network-ssd"
